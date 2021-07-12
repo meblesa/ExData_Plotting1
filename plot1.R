66820423010422c883a1568332c47bcd82d810fe
Maria B <53285616+meblesa@users.noreply.github.com>
2021-07-11 20:19:39 -0600

    Update plot1.R
    added reference to data download file

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
 #plot1
 png("plot1.png") 
+#see daytascript.R which contains the data download steps to get "Data3" dataset.
 hist(as.numeric(Data3[,3]),
      main="Global Active Power",
      col = "red",
