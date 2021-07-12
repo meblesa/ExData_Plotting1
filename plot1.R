e08e4f31efaedd28ecfd72c333c1762212e82648
Maria B <53285616+meblesa@users.noreply.github.com>
2021-07-11 20:34:14 -0600

    Update plot1.R
    added  width and height to png

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 #plot1
-png("plot1.png") 
+png("plot1.png", width = 480, height = 480) 
 #see daytascript.R which contains the data download steps to get "Data3" dataset.
 hist(as.numeric(Data3[,3]),
      main="Global Active Power",
