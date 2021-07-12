0aacd9ef5303deaf1168b5526df18000cb65712d
Maria B <53285616+meblesa@users.noreply.github.com>
2021-07-11 20:34:55 -0600

    Update plot2.R
    added png width and height

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ Data3[,1]<-as.Date(Data3[,1],format="%d/%m/%Y")
 
 Data3[,"Date Time"]<-as.POSIXct(paste(Data3[,1],Data3[,2]), format="%Y-%m-%d %H:%M:%S")
 
-png("plot2.png")
+png("plot2.png", width = 480, height = 480)
 
 plot(Data3[,10],Data3[,3],type="l",xlab="",ylab="Global Active Power (kilowatts)")
 
